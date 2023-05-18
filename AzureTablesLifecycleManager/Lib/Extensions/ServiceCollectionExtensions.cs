a38b01c1d390eea9785ef8fbd278607a85abad03
Pawel Flajszer <pflajszer@gmail.com>
2023-05-18 17:09:49 +0200

    Add another extension method for adding services to provide the connection string yourself; updated README

--- AzureTablesLifecycleManager/Lib/Extensions/ServiceCollectionExtensions.cs
+++ AzureTablesLifecycleManager/Lib/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Azure.Data.Tables;
 using AzureTablesLifecycleManager.AzureDAL.APIGateway;
 using AzureTablesLifecycleManager.Lib.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
@@ -11,6 +12,14 @@ namespace AzureTablesLifecycleManager.Lib.Extensions
         public static void AddAzureTablesLifecycleManagement(this IServiceCollection services)
         {
             services.AddSingleton(p => new TableServiceClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage")));
+            services.AddSingleton<ITableRepository, TableRepository>();
+            services.AddSingleton<ITableManager, TableManager>();
+            services.AddSingleton<IQueryBuilder, QueryBuilder>();
+        }
+
+        public static void AddAzureTablesLifecycleManagement(this IServiceCollection services, string connectionString)
+        {
+            services.AddSingleton(p => new TableServiceClient(connectionString));
             services.AddSingleton<ITableRepository, TableRepository>();
             services.AddSingleton<ITableManager, TableManager>();
             services.AddSingleton<IQueryBuilder, QueryBuilder>();
