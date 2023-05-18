d2e84ad6a490d6c26f7db2706a5642b3fe0aaccc
Pawel Flajszer <pflajszer@gmail.com>
2023-05-18 16:48:51 +0200

    Changed the way package is registered: using IServiceCollection extension and using Environment instead of IConfiguration. Old register method marked as obsolete

--- AzureTablesLifecycleManager/Lib/Extensions/FunctionsBuilderExtensions.cs
+++ AzureTablesLifecycleManager/Lib/Extensions/FunctionsBuilderExtensions.cs
@@ -4,17 +4,19 @@ using AzureTablesLifecycleManager.Lib.Services;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace AzureTablesLifecycleManager.Lib.Extensions
 {
-	public static class FunctionsBuilderExtensions
-	{
-		public static void RegisterAzureTablesLifecycleManagement(this IFunctionsHostBuilder builder)
-		{
-			builder.Services.AddSingleton(p => new TableServiceClient(p.GetService<IConfiguration>()["AzureWebJobsStorage"]));
-			builder.Services.AddSingleton<ITableRepository, TableRepository>();
-			builder.Services.AddSingleton<ITableManager, TableManager>();
-			builder.Services.AddSingleton<IQueryBuilder, QueryBuilder>();
-		}
-	}
+    public static class FunctionsBuilderExtensions
+    {
+        [Obsolete("RegisterAzureTablesLifecycleManagement method is deprecated. Use IServiceCollection extension method AddAzureTablesLifecycleManagement instead.")]
+        public static void RegisterAzureTablesLifecycleManagement(this IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton(p => new TableServiceClient(p.GetService<IConfiguration>()["AzureWebJobsStorage"]));
+            builder.Services.AddSingleton<ITableRepository, TableRepository>();
+            builder.Services.AddSingleton<ITableManager, TableManager>();
+            builder.Services.AddSingleton<IQueryBuilder, QueryBuilder>();
+        }
+    }
 }
---
+++ AzureTablesLifecycleManager/Lib/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Data.Tables;
+using AzureTablesLifecycleManager.AzureDAL.APIGateway;
+using AzureTablesLifecycleManager.Lib.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AzureTablesLifecycleManager.Lib.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void RegisterAzureTablesLifecycleManagement(this IServiceCollection services)
+        {
+            services.AddSingleton(p => new TableServiceClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage")));
+            services.AddSingleton<ITableRepository, TableRepository>();
+            services.AddSingleton<ITableManager, TableManager>();
+            services.AddSingleton<IQueryBuilder, QueryBuilder>();
+        }
+    }
+}
