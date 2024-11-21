dfa30436a9aaafadbdf85e8703317db5cbc8c277
Pawel Flajszer <pflajszer@gmail.com>
2024-11-21 20:49:50 +0100

    update test sdk and coverlet collector. surpressing warnings

--- AzureTablesLifecycleManager/Lib/Exceptions/TransferNotSuccessfulException.cs
+++ AzureTablesLifecycleManager/Lib/Exceptions/TransferNotSuccessfulException.cs
@@ -5,7 +5,9 @@ namespace AzureTablesLifecycleManager.Lib.Exceptions
 	public class TransferNotSuccessfulException : Exception
 	{
 		public TransferNotSuccessfulException() { }
+#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
 		public TransferNotSuccessfulException(string? message) : base(message) { }
-		public TransferNotSuccessfulException(string? message, Exception? innerException) : base(message, innerException) { }
-	}
+        public TransferNotSuccessfulException(string? message, Exception? innerException) : base(message, innerException) { }
+#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+    }
 }
